b37d3fcf45d9b1c829dbd6bcc170de769b68d2eb
AhmedSail <sdew2sdew0592855602@gmail.com>
2026-01-17 16:35:26 +0200

    checkout18

--- lib/email.ts
+++ lib/email.ts
@@ -4,23 +4,30 @@ export async function sendEmail({
   to,
   subject,
   text,
-
   html,
+  fromName,
+  replyTo,
 }: {
   to: string;
   subject: string;
   text: string;
   html?: string;
+  fromName?: string;
+  replyTo?: { email: string; name: string };
 }) {
   const client = new Brevo.TransactionalEmailsApi();
   client.setApiKey(
     Brevo.TransactionalEmailsApiApiKeys.apiKey,
-    process.env.BREVO_API_KEY!
+    process.env.BREVO_API_KEY!,
   );
 
   const email = {
-    sender: { email: "admin@orchida-ods.com", name: "orchida" }, // لازم يكون بريد من دومينك
+    sender: {
+      email: "admin@orchida-ods.com",
+      name: fromName || "orchida",
+    },
     to: [{ email: to }],
+    replyTo: replyTo,
     subject,
     textContent: text,
     htmlContent: html,
