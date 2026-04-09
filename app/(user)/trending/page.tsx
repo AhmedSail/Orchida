ca1308249a9b06e06cdc4737530bc86f0966c7ba
Ahmed Sail Abo Al-Qompoz <sdew2sdew0592855602@gmail.com>
2026-04-09 14:37:18 +0300

    feat: add sitemap generation and implement latest and trending pages with root layout configuration

--- app/(user)/trending/page.tsx
+++ app/(user)/trending/page.tsx
@@ -7,6 +7,9 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "المنتجات الرائجة | أوركيدة",
   description: "استعرض آخر المنتجات الرائجة في أوركيدة للخدمات الرقمية.",
+  alternates: {
+    canonical: "https://www.orchida-ods.com/trending",
+  },
 };
 
 export default async function TrendingPage() {
